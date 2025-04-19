bcd61c6e01ee04a527d5fb66019a417650c084d1
jeet_mandal <jeet22761@gmail.com>
2025-04-19 12:36:36 +0530

    work on websocket try to implement room joining logic

--- Metaverse/src/App.jsx
+++ Metaverse/src/App.jsx
@@ -1,13 +1,18 @@
-
-import EnvironmentComp from './comps/EnvironmentComp'
+// src/App.jsx
+import React from 'react';
+import {  Routes, Route } from 'react-router-dom';
+import Home from './comps/Home';
+import Room from './comps/Room';
 
 function App() {
-
   return (
-    <>
-      <EnvironmentComp />
-    </>
-  )
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room/:roomId" element={<Room />} />
+      </Routes>
+    
+  );
 }
 
-export default App
+export default App;
--- Metaverse/src/comps/EnvironmentComp.jsx
+++ Metaverse/src/comps/EnvironmentComp.jsx
@@ -1,18 +1,53 @@
+// src/comps/EnvironmentComp.jsx
 import { Canvas } from "@react-three/fiber";
-import { Environment, Gltf, OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { Classroom } from "../models/Classroom";
 import { Student1 } from "../models/Student1";
-export default function EnvironmentComp() {
+import { useEffect, useState } from "react";
+
+export default function EnvironmentComp({ users, socket }) {
+  const [userPositions, setUserPositions] = useState({});
+
+  useEffect(() => {
+    if (!socket) return;
+
+    // Listen for position updates from other users
+    socket.on('updatePosition', (data) => {
+      setUserPositions(prev => ({
+        ...prev,
+        [data.userId]: data.position
+      }));
+    });
+
+    // Send position updates
+    const interval = setInterval(() => {
+      // In a real implementation, you'd get the actual position of the user's avatar
+      socket.emit('positionUpdate', {
+        position: { x: 0, y: 0, z: 0 },
+      });
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [socket]);
+
   return (
     <Canvas>
       <OrbitControls />
-      {/* <Environment preset="city" color={"white"} /> */}
       <ambientLight intensity={1} color={"white"} />
       <directionalLight position={[0, 5, 0]} intensity={1} castShadow />
       <pointLight position={[0, 10, 10]} intensity={1} color={"white"} />
       <Classroom position={[0, 0, 0]} rotation={[0, Math.PI, 0]} />
-
-      <Student1 />
+      
+      {/* Render all users */}
+      {users.map((user) => (
+        <Student1 
+          key={user.id} 
+          position={userPositions[user.id] || [Math.random() * 4 - 2, 0, Math.random() * 4 - 2]} 
+          userId={user.id}
+        />
+      ))}
     </Canvas>
   );
 }
